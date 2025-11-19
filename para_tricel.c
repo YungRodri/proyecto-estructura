ce3f31218dba7e1743f2fb68706ecf9379b1e680
marokiyuria0 <huertahenrry127@gmail.com>
2025-11-19 15:58:33 -0300

    supuestamente el definitivo xd

--- para_tricel.c
+++ para_tricel.c
@@ -1,23 +1,4 @@
-truct Resultado {
-    struct Eleccion  *eleccion;   /* referencia a la elección */
-    struct Candidato *ganador;    /* puntero a uno de eleccion->cands[idxGanador] */
-
-    int   totalMesas;
-    int   totalVotantesRegistrados;
-    int   totalVotantesVotaron;
-    int   votosValidos;
-    int   votosBlancos;
-    int   votosNulos;
-    float porcentajeParticipacion;               /* 0..100 */
-    float porcentajeCandidato[MAX_CANDIDATOS];   /* solo 0..(nCands-1) */
-    int   idxGanador;                            /* índice dentro del arreglo compacto */
-    float porcentajeGanador;
-
-    struct Resultado *sig;   /* anillo: lista circular simplemente enlazada */
-};
-
-
-
+//------------------------------------------------------------------------------------------------------------------------------------------------------
 
 
 void ContarMesas(struct NodoMesa *mesas, int *contador){
@@ -83,14 +64,19 @@ int ValidarSegundaVuelta(struct Resultado *resultados, struct Eleccion *eleccion
 void mostrarResultados(struct Resultado *resultados){
     int i;
 
+    printf("ID de la eleccion = %d\n",resultados->eleccion->id);
     printf("total de mesas = %d\n", resultados->totalMesas);
     printf("total de votantes registrados = %d\n", resultados->totalVotantesRegistrados);
     printf("total de votos = %d\n", resultados->totalVotantesVotaron);
     printf("total de votos nulos = %d\n", resultados->votosNulos);
     printf("total de votos blancos = %d\n", resultados->votosBlancos);
-    printf("total de votos efectivos = %d\n", resultados->votosValidos);
+    printf("total de votos efectivos = %d\n\n", resultados->votosValidos);
 
-    printf("situacion de elecciones = Un candidato superó el 50%%, elecciones terminadas.\n");
+    printf("situacion de elecciones = Un candidato superó el 50%%, elecciones terminadas.\n\n");
+    printf("GANADOR ELECCION\n");
+    printf("ID DEL GANADOR = %d\n",resultados->ganador->id);
+    printf("NOMBRE DEL GANADOR = %s\n",resultados->ganador->datos->nombre);
+    printf("PORCENTAJE DEL GANADOR = %f\n\n",resultados->porcentajeGanador);
 
 }
 
@@ -167,14 +153,17 @@ struct Resultado* recopilarResultados(struct SistemaElectoral *sistema,struct El
 
     if (segunda_vuelta == 1){
         printf("situacion de elecciones = No hay candidatos que superen el 50%% de votos, habrá una segunda vuelta........\n\n");
-        int idX =-1, idY=-1;
+        int idX =-1, idY=-1, k;
         struct Eleccion *segunda = malloc(sizeof(struct Eleccion));
         struct Resultado *ResultadoSegunda;
-        segunda->cands = malloc(sizeof(struct Candidato*) * 2);
+        
+        for (k = 0; k < MAX_CANDIDATOS; k++) {
+            segunda->cands[k] = NULL;
+        }
 
         paraSegundaVuelta(final,eleccionActual,&idX, &idY);
+        
         segunda->nCands = 2;
-
         segunda->cands[0]=eleccionActual->cands[idX];
         segunda->cands[1]=eleccionActual->cands[idY];
         segunda->arbolMesas = eleccionActual->arbolMesas;
@@ -200,6 +189,15 @@ void  agregarAtricel(struct Tricel * tricel, struct Resultado *resultadoNuevo){
         resultadoNuevo->sig = resultadoNuevo;
         return;
     }
+
+    struct Resultado *aux = tricel->headResultados;
+
+    do{
+        aux=aux->sig;
+    }while(aux->sig !=tricel->headResultados);
+
+    aux->sig = resultadoNuevo;
+    resultadoNuevo->sig = tricel->headResultados;
 }
 
 
@@ -226,4 +224,134 @@ void resultadoEleccionXid (struct Tricel *sistema, int idBuscado){
 
 }
 
-void proclamarUnGanador (struct)
+void proclamarUnGanador (struct Tricel *tricel){
+
+    struct Resultado *rec =tricel->headResultados->sig;
+    struct Resultado *head = tricel->headResultados;
+
+    while(rec->sig!=head){
+        rec = rec->sig;
+    }
+
+    printf("GANADOR ELECCION DE LAS ULTIMAS ELECCIONES REALIZADAS\n\n");
+    printf("ID DEL GANADOR = %d\n",rec->ganador->id);
+    printf("NOMBRE DEL GANADOR = %s\n",rec->ganador->datos->nombre);
+    printf("PORCENTAJE DEL GANADOR = %f\n\n",rec->porcentajeGanador);
+}
+
+
+
+void listarResultado (struct Tricel *tricel){
+
+    if (tricel->headResultados == NULL){
+        printf("NO HAY RESULTADOS EN EL REGISTRO\n\n");
+        return;
+    }
+
+    printf("LISTA DE RESULTADOS REGISTRADOS EN EL TRICEL\n\n");
+    struct Resultado *rec = tricel->headResultados;
+    
+    do{
+
+        mostrarResultados(rec);
+        rec=rec->sig;
+    }while(rec!=tricel->headResultados);
+
+    printf("\n\n");
+
+    printf("resultados listados\n\n");
+
+
+}
+
+
+
+
+
+
+
+void MenuTricel( struct Tricel *tricel, struct SistemaElectoral *sistema)
+{
+    int indice = - 1;
+
+    do
+    {
+        printf("\n-- TRICEL --\n");
+        printf("1) Generar resultados desde Eleccion\n"); // -> (Esto acumula ABB de mesas)
+        printf("2) Listar Resultados\n" );
+        printf("3) Ver resultado por Id de ELECCION\n");
+        printf("4) Proclamar Ganador\n");
+        if (scanf("%d", &indice) != 1)
+        {
+            indice = - 1;
+            continue;
+
+        }
+        switch (indice)
+        {
+            case 1:
+            {
+                int idB;
+                printf("Ingrese id de la eleccion a buscar\n\n");
+                scanf("%D",&idB);
+
+                struct Eleccion *aux =sistema->servel->elecciones;
+
+                while (aux != NULL && aux->id != idB){
+                    aux = aux->sig;
+                }
+
+                if (aux == NULL){
+                    printf("No existe una elección con ese ID.\n");
+                    break;
+                }
+
+                struct Resultado *resultado = recopilarResultados(sistema, aux);
+
+                agregarAtricel(tricel, resultado);
+
+
+                printf("Generar Resultado, aun no \n");
+                break;
+            }
+            case 2:
+            {
+
+                listarResultado(tricel);
+                printf("Listar Resultados, aun no  \n");
+                break;
+
+            }
+            case 3:
+            {
+                int idB;
+                printf("Ingrese el ID de la eleccion que desee buscar\n\n");
+                scanf("%d",&idB);
+                resultadoEleccionXid(tricel,idB);
+                printf("Ver resultado por id");
+                break;
+
+            }
+            case 4:
+            {
+                proclamarUnGanador(tricel);
+                printf("Ganador proclamado\n");
+                break;
+
+            }
+            case 0:
+            {
+                printf("BREAK");
+                break;
+
+            }
+        }
+
+
+    }while (indice != 0);
+
+}
+
+
+//---------------------------------------------------------------------------------------------------
+
